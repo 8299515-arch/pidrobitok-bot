edb8cd9c416bbdf35561d9ca9f3d6ea638503fca
8299515-arch <8299515@gmail.com>
2026-05-06 00:27:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -131,11 +131,9 @@ def main():
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("post", post))
-
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
 
-    app.run_polling()
+    print("🚀 STARTING BOT...")
 
-if __name__ == "__main__":
-    main()
+    app.run_polling()
    
